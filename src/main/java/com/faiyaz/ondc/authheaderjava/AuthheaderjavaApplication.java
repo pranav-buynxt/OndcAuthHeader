6a03d8dd3ec1aec61aa56b9fbfad61845b6aefe3
pranav-buynxt <pranav@buynxt.in>
2023-03-26 20:47:22 +0530

    first commit

---
+++ src/main/java/com/faiyaz/ondc/authheaderjava/AuthheaderjavaApplication.java
@@ -0,0 +1,28 @@
+package com.faiyaz.ondc.authheaderjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@RestController
+public class AuthheaderjavaApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthheaderjavaApplication.class, args);
+		System.out.println("Program starts from here...");
+		
+		
+	}
+
+	@GetMapping("/")
+	public String getHomePage() {
+		return "Working Fine...!";
+	}
+
+
+}
